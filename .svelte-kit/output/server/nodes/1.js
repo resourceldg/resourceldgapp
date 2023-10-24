f008e6ac5443de665cc8b5da58fba1b171a8f6ed
resourceldg <resourceldg@gmail.com>
2023-10-24 13:48:08 -0300

    with typescript

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.38955e2f.js","_app/immutable/chunks/scheduler.cbf234a0.js","_app/immutable/chunks/index.200976ee.js","_app/immutable/chunks/stores.70be97f1.js","_app/immutable/chunks/singletons.404b1b0b.js","_app/immutable/chunks/index.14349a18.js"];
-export const stylesheets = [];
-export const fonts = [];
